4fd8d0f0a1e7382d8f446cfd676fb1750f0a59cb
maik001 <maikemir2003@gmail.com>
2023-12-30 00:40:03 -0300

    criacao do helper app

--- index.php
+++ index.php
@@ -3,6 +3,15 @@ declare(strict_types = 1);
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$config = \App\Helpers\Config::get('app');
+$app = new App\Helpers\App();
 
-var_dump($config);
+echo $app->getServerTime()->format('Y-m-d H:i:s') . PHP_EOL;
+echo $app->getLogPath(). PHP_EOL;
+echo $app->getEnviroment(). PHP_EOL;
+echo $app->isDebugMode(). PHP_EOL;
+
+if($app->isRunningFromConsole()) {
+    echo 'from console';
+} else {
+    echo 'from browser';
+}
