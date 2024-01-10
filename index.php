34e1577b8e5f4504a409873709d73a1266e543aa
maik001 <maikemir2003@gmail.com>
2024-01-10 00:16:04 -0300

    implementando psr 3 logger interface

--- index.php
+++ index.php
@@ -4,19 +4,11 @@ declare(strict_types = 1);
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/src/exception/Exception.php';
 
-$db = new mysqli('dsfsdgs', 'root', '', 'bug');
+$logger = new \App\Logger\Logger();
 
-$config = App\Helpers\Config::getFileContent('yeeyey');
-var_dump($config);
+$logger->log(
+    \App\Logger\LogLevel::EMERGENCY,
+    'there is a emergency situation',
+    ['exception' => 'exception occured']);
 
-$app = new App\Helpers\App();
-echo $app->getServerTime()->format('Y-m-d H:i:s') . PHP_EOL;
-echo $app->getLogPath(). PHP_EOL;
-echo $app->getEnviroment(). PHP_EOL;
-echo $app->isDebugMode(). PHP_EOL;
-
-if($app->isRunningFromConsole()) {
-    echo 'from console';
-} else {
-    echo 'from browser';
-}
+$logger->info("User account created successfully", ['id' => 5]);
---
+++ src/logger/LogLevel.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Logger;
+
+abstract class LogLevel
+{
+    const DEBUG = 'debug';
+    const INFO = 'info';
+    const NOTICE = 'notice';
+    const WARNING = 'warning';
+    const ERROR = 'error';
+    const CRITICAL = 'critical';
+    const ALERT = 'alert';
+    const EMERGENCY = 'emergency';
+}
